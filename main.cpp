6caa2ec0cb3a65fb49335f2c9db4bd6cb3a03cad
Mykol-Word <142253711+Mykol-Word@users.noreply.github.com>
2025-02-18 16:20:49 -0800

    basic task adding repl

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <windows.h>
 #include <string> 
 
 using namespace std;
@@ -7,10 +8,39 @@ using namespace std;
 //Sort of like a task-list repl
 int main()
 {
-    string s;
-    cout << "Enter string" << endl;
-    cin >> s;
-    cout << "Hello " + s << endl;
+    HANDLE  hConsole;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    return 0;
+    ifstream task_in;
+    ofstream task_out;
+
+    while(true)
+    {
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+        cout << "Add a task: ";
+        SetConsoleTextAttribute(hConsole, 15);
+
+        string new_task;
+        getline(cin, new_task);
+
+        printf("\033c");
+
+        task_out.open("tasks.txt", fstream::app);
+        int task_length = new_task.length();
+        task_out << new_task;
+        task_out << endl;
+        task_out.close();
+
+        task_in.open("tasks.txt");
+        if (task_in.is_open())
+        {
+            char current_char;
+            while(task_in)
+            {
+                current_char = task_in.get();
+                cout << current_char;
+            }
+        }
+        task_in.close();
+    }
 }
